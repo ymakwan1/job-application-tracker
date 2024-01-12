29f63d64b532c948d0d87f6a356de3819172f806
ymakwan1 <ymakwan1@binghamton.edu>
2024-01-12 20:18:19 +0530

    using env file

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
-# backend/app.py
 from flask import Flask
 from flask_cors import CORS
 from models import db
 from routes import init_routes
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:3000", methods=["GET", "POST", "PUT", "DELETE"])
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yashmakwana@localhost:5432/job_db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yashmakwana:9802@postgres:5432/job_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -19,7 +21,3 @@ def init_db():
     with app.app_context():
         db.create_all()
         db.session.commit()
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=False, port=5000)
